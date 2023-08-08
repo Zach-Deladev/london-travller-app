750edd6c1c26ee1b08a3f8c41f3e7c4e26a99df6
woltel <32892141+woltel@users.noreply.github.com>
2023-08-08 18:33:29 +0100

    White logo added to master by Teo.

---
+++ app/src/main/java/com/example/loginsignupsystem/controller/ConfirmationFragment.java
@@ -0,0 +1,18 @@
+package com.example.loginsignupsystem.controller;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.viewmodel.CreationExtras;
+
+public class ConfirmationFragment extends Fragment {
+    public void setArguments(Bundle args) {
+    }
+
+    @NonNull
+    @Override
+    public CreationExtras getDefaultViewModelCreationExtras() {
+        return super.getDefaultViewModelCreationExtras();
+    }
+}
