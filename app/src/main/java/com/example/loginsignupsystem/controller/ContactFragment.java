f3ed3c14c1318c828821c892ee351a73f672d958
belalhosany <131386266+belalhosany@users.noreply.github.com>
2023-08-10 22:18:41 +0100

    committing change to phone problem done

--- app/src/main/java/com/example/loginsignupsystem/controller/ContactFragment.java
+++ app/src/main/java/com/example/loginsignupsystem/controller/ContactFragment.java
@@ -28,7 +28,8 @@ public class ContactFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        View view = inflater.inflate(R.layout.activity_contact, container, false);
 
         pht = view.findViewById(R.id.pht);
         button = view.findViewById(R.id.btnsend);
